5a9a6d408bd9652c09b8603c2fae08be73a097f6
Abdullah Ali <abdullah.dev3@gmail.com>
2021-02-08 21:02:25 +0300

    Edit user info form

--- resources/views/user/form.blade.php
+++ resources/views/user/form.blade.php
@@ -1,68 +1,77 @@
 @extends('layouts.app')
 @section('content')
-<div class="container">
-	<form action="/user/update" method="post" accept-charset="utf-8">
-			@csrf
+    <div class="container">
+        <form action="/user/update" method="post" accept-charset="utf-8">
+            @csrf
             <div class="form-group">
                 <label for="national_id">رقم الهوية</label>
-                <input type="text" class="form-control p-1 m-1" disabled id="national_id" name="national_id" value="{{ $user->national_id }} ">
+                <label type="text" class="form-control p-1 m-1 bg-light"  id="national_id" name="national_id"
+                    >{{ $user->national_id }}</label>
             </div>
 
             <div class="form-group">
                 <label for="name">الاسم</label>
-                <input type="text" class="form-control p-1 m-1" disabled id="name" name="name" value="{{ $user->name }} ">
+                <label type="text" class="form-control p-1 m-1  bg-light"  id="name" name="name">{{ $user->name }}</label>
             </div>
 
             <div class="form-group">
                 <label for="phone">رقم الجوال</label>
-                <input required type="phone" class="form-control p-1 m-1" id="phone" name="phone" value="{{ $user->phone }} ">
+				<div class="input-group mb-3">
+                <input required disabled="true" type="phone" class="form-control p-1 m-1" id="phone" name="phone"
+                    value="{{ $user->phone }} ">
+					<button onclick="return document.getElementById('phone').disabled=false" class="btn btn-sm px-2 m-1 btn-primary font-weight-bold">تعديل</button>
+				</div>
             </div>
 
             <div class="form-group">
                 <label for="email">البريد الالكتروني</label>
-                <input required type="email" class="form-control p-1 m-1" id="email" name="email" value="{{ $user->email }} ">
+                <input required type="email" class="form-control p-1 m-1" id="email" name="email"
+                    value="{{ $user->email }} ">
             </div>
 
             <div class="form-row">
 
-            	<div class="col-sm-4">
-            		<label for="department" class=""> القسم </label>
-                	<input required disabled type="text" class="form-control" id="department" name="department" value="{{ $user->department->name }} ">
-            	</div>
+                <div class="col-sm-4">
+                    <label for="department" class=""> القسم </label>
+                    <label required  type="text" class="form-control  bg-light" id="department" name="department">{{ $user->department->name }}</label>
+                </div>
 
-            	<div class="col-sm-4">
-            		<label for="major" class=""> التخصص </label>
-                	<input required disabled type="text" class="form-control" id="major" name="major" value="{{ $user->major->name }} ">
-            	</div>
+                <div class="col-sm-4">
+                    <label for="major" class=""> التخصص </label>
+                    <label required  type="text" class="form-control  bg-light" id="major" name="major">{{ $user->major->name }}</label>
+                </div>
 
-							<div class="col-sm-4">
-									<label for="cost"> المبلغ المراد سداده</label>
-									<input required disabled type="text" class="form-control" id="cost" name="cost" value="{{ $user->major->cost }} ">
-							</div>
+                <div class="col-sm-4">
+					<label for="cost"> المبلغ المراد سداده</label>
+					<div class="input-group mb-3">
+						<label required  type="text" class="form-control  bg-light" id="cost" name="cost">{{ $user->major->cost }}</label>
+						<span class="input-group-text">SR</span>
+					  </div>
+                </div>
             </div>
 
 
 
-						<div class="form-group">
-							<label for="">صورة الهوية الوطنية </label>
-							<input type="file" name="" class="form-control" value="">
-						</div>
+            <div class="form-group">
+                <label for="">صورة الهوية الوطنية </label>
+                <input type="file" name="" class="form-control" value="">
+            </div>
 
-						<div class="form-group">
-							<label for="">صورة من المؤهل </label>
-							<input type="file" name="" class="form-control" value="">
-						</div>
+            <div class="form-group">
+                <label for="">صورة من المؤهل </label>
+                <input type="file" name="" class="form-control" value="">
+            </div>
 
-						<div class="form-group">
-							<label for=""> صورة الايصال </label>
-							<input type="file" name="" class="form-control" value="">
-						</div>
+            <div class="form-group">
+                <label for=""> صورة الايصال </label>
+                <input type="file" name="" class="form-control" value="">
+            </div>
 
             <div class="form-group my-3">
                 <input type="submit" name="form_submit" id="excel_submit" value="أرسال" class="btn btn-primary">
             </div>
-	</form>
-</div>
-
+        </form>
+    </div>
+    </div>
 
 @stop
