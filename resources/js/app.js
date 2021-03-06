c3bd9da273826db5d85881f91208eb39ff049f40
Abdullah Ali <abdullah.dev3@gmail.com>
2021-03-06 23:52:01 +0300

    Remove datatable lib and use cdn

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,5 @@ require("./studentsDocumentsReview");
 require("./studentFinalAccepted");
 require("./studentForm");
 require("./excelForm");
-import * as dt from 'datatables.net';
 import Swal from "sweetalert2";
 window.Swal = Swal;
