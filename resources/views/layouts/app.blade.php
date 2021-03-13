927b4b4ab7ae7caa0beaeae9d59376f124b53e5c
Abdullah Ali <abdullah.dev3@gmail.com>
2021-03-14 02:30:16 +0300

    commit for upload to server

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -92,13 +92,14 @@
                                     {{-- <li class="nav-item"><a class="nav-link" href="https://ugate.tvtc.gov.sa/AFrontGate/">البوابة الإلكترونية للقبول</a></li>
                                     <li class="nav-item"><a class="nav-link" href="https://tvtc.gov.sa/pdf/TVTC-at-a-Glance-AR.pdf">تعرف علينا </a></li> --}}
                                 </ul>
-                                {{-- <a class="navbar-brand" href="https://www.tvtc.gov.sa/" target="_blank">
-                                    <img style="width: 250px;" class="navbar-brand"  src="{{asset('images/tvtclogo1.svg')}}" alt="" />
-                                </a> --}}
-                                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                                    <span class="navbar-toggler-icon"></span>
-                                </button>
+
                             </div>
+                            <a class="navbar-brand" href="https://www.tvtc.gov.sa/" target="_blank">
+                                {{-- <img style="width: 250px;" class="navbar-brand"  src="{{asset('images/tvtclogo1.svg')}}" alt="" /> --}}
+                            </a>
+                            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                                <span class="navbar-toggler-icon"></span>
+                            </button>
                         </nav>
                     </div>
                 </div>
