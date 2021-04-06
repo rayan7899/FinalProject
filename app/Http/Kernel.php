8740db77f74f556520cf87e7e914dd5273352cf8
Muhannad Alrusayni <Muhannad.Alrusayni@gmail.com>
2021-04-06 23:51:51 +0300

    Add EnsureHasRole middleware class proto-type

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,6 +63,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'agreement' => \App\Http\Middleware\Agreement::class,
+        'role' => \App\Http\Middleware\EnsureManagerHasRole::class,
         'errors'    => \Illuminate\View\Middleware\ShareErrorsFromSession::class,
     ];
 }
