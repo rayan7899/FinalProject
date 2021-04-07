f74d3a36c7a9ee5178725cd544cd4da57f8e35c1
rayan <rayan7899@hotmail.com>
2021-04-07 07:11:18 +0300

    fix checked student form

--- resources/views/manager/studentsAffairs/CheckedStudents.blade.php
+++ resources/views/manager/studentsAffairs/CheckedStudents.blade.php
@@ -1,6 +1,24 @@
 @extends('layouts.app')
 @section('content')
 <div class="container">
+  <div class="modal fade" id="pdfModal" tabindex="-1" role="dialog" aria-labelledby="modalLabel" aria-hidden="true">
+    <div class="modal-dialog" style="max-width: 75%" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="pdfName"></h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <iframe id="pdfIfreme" src="" width="100%" height="600px"></iframe>
+                <div class="text-center">
+                    <img id="modalImage" src="" alt="image" class="img-fluid" />
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 المتدربين المدققة ايصالاتهم
 
       <table class="table nowrap display cell-border" id="mainTable">
@@ -15,6 +33,7 @@
             <th scope="col">التخصص</th>
             <th scope="col">الحالة</th>
             <th scope="col">ملاحظات المدقق</th>
+            <th scope="col">الايصالات</th>
           </tr>
         </thead>
         <tbody>
@@ -30,6 +49,26 @@
                 <td>{{ $user->student->major->name ?? 'لا يوجد' }} </td>
                 <td>{{ __($user->student->traineeState) ?? 'لا يوجد' }} </td>
                 <td>{{ $user->student->note ?? 'لا يوجد' }} </td>
+                <td class="text-center">
+
+                    @php
+                        $splitByDot = explode('.', $user->student->receipt);
+                        $fileExtantion = end($splitByDot);
+                    @endphp
+                    @if ($fileExtantion == 'pdf' || $fileExtantion == 'PDF')
+                        <a data-toggle="modal" data-target="#pdfModal" href="#"
+                            onclick="showPdf('{{ route('GetStudentDocument', ['path' => $user->student->receipt]) }}','pdf')">
+                            <img style="width: 20px" src="{{ asset('/images/pdf.png') }}" />
+                        </a>
+                    @else
+                        <a data-toggle="modal" data-target="#pdfModal" href="#"
+                            onclick="showPdf('{{ route('GetStudentDocument', ['path' => $user->student->receipt]) }}','img')">
+                            <img src=" {{ asset('/images/camera_img_icon.png') }}" style="width:25px;"
+                                alt="Image File">
+                        </a>
+                    @endif
+
+                </td>
               </tr>
             @empty
               <td colspan="9"> لا يوجد بيانات</td>
