777c6bd3977179bd50e88ac6033a55558bfa4114
Abdullah ali <abdullah.dev3@gmail.com>
2021-05-23 02:54:34 +0300

    Add help to login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,7 +12,7 @@
                         @csrf
 
                         <div class="form-group row">
-                            <label for="national_id" class="col-md-4 col-form-label text-md-right">{{ __('أسم المستخدم') }}</label>
+                            <label for="national_id" class="col-md-4 col-form-label text-md-left">{{ __('أسم المستخدم') }}</label>
 
                             <div class="col-md-6">
                                 <input id="national_id" type="text" class="form-control @error('national_id') is-invalid @enderror" name="national_id" value="{{ old('national_id') }}" required autocomplete="number" autofocus>
@@ -26,7 +26,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('كلمة المرور') }}</label>
+                            <label for="password" class="col-md-4 col-form-label text-md-left">{{ __('كلمة المرور') }}</label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
@@ -38,14 +38,15 @@
                                 @enderror
                             </div>
                         </div>
-
+                      
                         <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
+                            <div class="col-md-8 offset-md-4 ">
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('تسجيل الدخول') }}
                                 </button>
+                                <a class="mx-4" href="{{asset('help.pdf')}}" target="_blank">تعليمات الاستخدام</a>
 
-                                {{-- @if (Route::has('password.request'))
+                                {{-- @if (Route::has('password.request' ))
                                     <a class="btn btn-link" href="{{ route('password.request') }}">
                                         {{ __('Forgot Your Password?') }}
                                     </a>
