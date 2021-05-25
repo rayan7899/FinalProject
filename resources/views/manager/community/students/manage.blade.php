fa307a2dccb17706814ee68099ac7e50dd1a9352
Abdullah ali <abdullah.dev3@gmail.com>
2021-05-26 01:56:34 +0300

    Edit title

--- resources/views/manager/community/students/manage.blade.php
+++ resources/views/manager/community/students/manage.blade.php
@@ -47,7 +47,7 @@
                         <a href="{{ route('getStudentForm') }}" class="btn btn-outline-primary p-3 m-2"
                         style="font-size: 16px; width: 220px;">جميع طلبات و بيانات المتدرب</a>
                         <a href="{{ route('exportMainStudentDataExcel') }}" class="btn btn-outline-primary p-3 m-2"
-                        style="font-size: 16px; width: 220px;">بيانات جميع الطلاب اكسل</a>
+                        style="font-size: 16px; width: 220px;">بيانات جميع المتدربين اكسل</a>
                 </div>
 
             </div>
