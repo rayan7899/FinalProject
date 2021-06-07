65964c0b7879aefd05b369880226052213a1c4f2
Abdullah ali <abdullah.dev3@gmail.com>
2021-06-07 17:52:09 +0300

    Rename credit_hours to available_hours and create credit_hours column

---
+++ database/migrations/2021_06_07_165403_students_add_available_hours_column.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class StudentsAddAvailableHoursColumn extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('students', function (Blueprint $table) {
+            $table->integer('credit_hours')->nullable()->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('students', function (Blueprint $table) {
+            //
+        });
+    }
+}
